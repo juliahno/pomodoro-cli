45195985849f94793007c16a3ee7cb1db27f2023
Juliano <43688750+juliahno@users.noreply.github.com>
2022-10-18 20:35:46 -0300

    Update pomo.sh

--- pomo.sh
+++ pomo.sh
@@ -1,13 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-arg1=$1
-shift
-args="$*"
-                                               
-min=${arg1:?Example: pomo 15 Take a break}
-sec=$((min * 60))
-msg="${args:?Example: pomo 15 Take a break}"
+session_minutes=${1:-25}
+break_minutes=${2:-5}
 
-while true; do
-    date '+%H:%M' && sleep "${sec:?}" && notify-send -u critical -t 0 -a pomo "${msg:?}"
+session_seconds=$((session_minutes * 60))
+break_seconds=$((break_minutes * 60))
+
+while true
+do
+    date '+%H:%M'
+    sleep $session_seconds
+    zenity --info --title="Pomodoro" --text="End of session.\nOK starts the break."
+    
+    date '+%H:%M'
+    sleep $break_seconds
+    zenity --info --title="Pomodoro" --text="End of break.\nOK starts the session."
 done
